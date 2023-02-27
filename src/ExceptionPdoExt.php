057db68307b5e1b8f7ce91f6204ab0c6f50e39f4
yusam <yusam@yandex.ru>
2023-02-27 08:21:07 +0400

    #changed

---
+++ src/ExceptionPdoExt.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace YusamHub\DbExt;
+
+class ExceptionPdoExt extends \Exception
+{
+    protected string $lastSql;
+    protected array $lastBindings;
+    protected int $affectedRows;
+    protected ?int $lastInsertId;
+
+    /**
+     * @param PdoExt $pdoExt
+     * @param string $message
+     * @param int $code
+     * @param \Throwable|null $previous
+     */
+    public function __construct(PdoExt $pdoExt, string $message = "", int $code = 0, \Throwable $previous = null)
+    {
+        $this->lastSql = $pdoExt->getLastSql();
+        $this->lastBindings = $pdoExt->getLastBindings();
+        $this->affectedRows = $pdoExt->affectedRows();
+        $this->lastInsertId = $pdoExt->lastInsertId();
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastSql(): string
+    {
+        return $this->lastSql;
+    }
+
+    /**
+     * @return array
+     */
+    public function getLastBindings(): array
+    {
+        return $this->lastBindings;
+    }
+
+    /**
+     * @return int
+     */
+    public function affectedRows(): int
+    {
+        return $this->affectedRows;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function lastInsertId(): ?int
+    {
+        return $this->lastInsertId;
+    }
+}
